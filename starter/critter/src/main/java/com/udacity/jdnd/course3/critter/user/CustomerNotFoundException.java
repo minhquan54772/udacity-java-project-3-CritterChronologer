a429a441c92cdec91d79d2c662f67c81b86d808a
NMQ <ngminhquan.aries@gmail.com>
2024-08-07 23:14:28 +0700

    Add entity models

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerNotFoundException.java
@@ -0,0 +1,10 @@
+package com.udacity.jdnd.course3.critter.user;
+
+public class CustomerNotFoundException extends RuntimeException{
+    public CustomerNotFoundException() {
+    }
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
